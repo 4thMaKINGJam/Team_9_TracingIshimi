c5a84ac98acc8b4372011d1fd3c4a78cb2f2f3bf
SominLee <shl_4869@ewhain.net>
2023-11-13 19:47:15 +0900

    True Final

--- TracingIshimi/Assets/Scripts/EndingStory/Credit.cs
+++ TracingIshimi/Assets/Scripts/EndingStory/Credit.cs
@@ -13,7 +13,7 @@ public class Credit : MonoBehaviour
         panel_back.SetActive(true);
         panel_end.SetActive(true);
         END_text.text = "제 4회 메이킹잼\n프로젝트 - 이시미잡이\n\n- 기획 및 사운드 -\n<color=\"grey\">한국음악과</color>\n한이새꽃\n\n- 그래픽 -\n<color=\"grey\">섬유예술과</color>\n이주원\n\n- 코더 -\n<color=\"grey\">컴퓨터공학전공</color>\n김정민\n\n김현민\n\n이소민";
-        gameObject.GetComponent<RectTransform>().anchoredPosition = new Vector3(300f,-500f);
+        gameObject.GetComponent<RectTransform>().anchoredPosition = new Vector3(0,-500f);
         panel_end.GetComponent<Image>().color = new Color(0,0,0,1);
     }
     void Update(){
@@ -26,7 +26,7 @@ public class Credit : MonoBehaviour
                 panel_end.SetActive(false);
             }
          float posY = gameObject.GetComponent<RectTransform>().anchoredPosition.y;
-         gameObject.GetComponent<RectTransform>().anchoredPosition = new Vector3(300f,posY+2f);
+         gameObject.GetComponent<RectTransform>().anchoredPosition = new Vector3(0,posY+2f);
         }
     }
 }
